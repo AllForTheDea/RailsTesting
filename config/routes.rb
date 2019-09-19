ed1295672b4b8441a95eeaf9e3f9454ac052380b
AllForTheDea <dragonfleet@gmail.com>
2019-09-19 10:57:31 -0400

    added routes

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'index' => 'welcome#index'
+  get 'about' => 'welcome#about'
+  root 'welcome#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
